e3ceac526d5aab26cb67e6de68dcf918b6a47a7c
Precious00001 <lanremidelola@gmail.com>
2024-07-10 06:06:58 +0100

    setting my website to use media

--- website/web/settings.py
+++ website/web/settings.py
@@ -119,6 +119,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = (BASE_DIR / 'static')
+MEDIA_ROOT = (BASE_DIR / 'media')
+MEDIA_URL = '/media/'
+
 
 
 # Default primary key field type
