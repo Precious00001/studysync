b371f270184223c53f509ad22903c74d8e1f459b
Precious00001 <lanremidelola@gmail.com>
2024-07-10 13:32:51 +0100

    connecting app to project

--- website/web/settings.py
+++ website/web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'discord',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,8 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTH_USER_MODEL = 'discord.CustomUser'
 
 WSGI_APPLICATION = 'web.wsgi.application'
 
