6a1111be4dd4260ae837de4071ef129ae77f030b
Precious <lanremidelola@gmail.com>
2024-07-30 03:17:48 +0000

    adding urls for creating room

--- website/discord/urls.py
+++ website/discord/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
 
     # Room management URLs
     path('create-room/', views.createRoom, name="create-room"),  # URL for creating a new room
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
 
    # User profile management URL
     path('update-user/', views.updateUser, name="update-user"),  # URL for updating user profile
